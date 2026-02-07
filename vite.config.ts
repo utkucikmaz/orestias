dd6b82cb018bc5776d2fe3913a07379df3c8ba8b
utkucikmaz <utkucikmaz@gmail.com>
2026-02-07 23:40:23 +0100

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/orestias/",
   plugins: [react()],
   build: {
+    outDir: "docs",
     chunkSizeWarningLimit: 1200,
   },
 });
